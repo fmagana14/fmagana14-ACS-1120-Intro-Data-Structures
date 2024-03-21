f3eb088801a8abc19a941e42f49cfbc761d3e7a5
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2024-03-20 17:33:49 -0700

    completed dictogram and hashtables files

--- Code/dictogram.py
+++ Code/dictogram.py
@@ -21,15 +21,30 @@ class Dictogram(dict):
     def add_count(self, word, count=1):
         """Increase frequency count of given word by given count amount."""
         # TODO: Increase word frequency by count
+        if word.lower() in self:
+            self[word] += count
+        else:
+            self.update({word.lower(): count})
+            self.types += 1
+
+            self.tokens += count
 
     def frequency(self, word):
         """Return frequency count of given word, or 0 if word is not found."""
         # TODO: Retrieve word frequency count
+        word_frequency = self.get(word, 0)
+        return word_frequency
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
+        random_word = random.choices(
+            list(self.keys()), 
+            weights = self.values(),
+            k = 1) [0] # changed dictionary to list in order to avoid a depeciation warning 
+        return random_word
+
 
 
 def print_histogram(word_list):
